8e341a3673ef5d794c321ed6661ee279bf2f3367
tastibip <tastibip@gmail.com>
2025-12-19 17:03:34 +0700

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-
-FILE_PATH = "D:/OneDrive - TASTI/BIPShare/Dashboard of Anything/New BIP Dash 2.4.xlsx"
-
-SHEET_NAME_REVKAB = "RevbyKab"
-SHEET_NAME_REVNAT = "RevbyNat"
-SHEET_NAME_REVPROV = "RevbyProv"
-SHEET_NAME_PNVAR = "PN Varians"
-SHEET_NAME_TOPPART = "Top10Part"
